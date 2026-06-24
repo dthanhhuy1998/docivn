0bcc4356bfd22c82ecf1e81057251e4101ee06d0
Huy Dao <dthanhhuy1998@gmail.com>
2026-06-24 15:08:22 +0700

    fix: erorr auto redirect from method GET to POST in upload filepond

--- resources/js/admin/product/index.js
+++ resources/js/admin/product/index.js
@@ -66,9 +66,9 @@ class ProductIndexPage {
             chunkForce: true,
 
             server: filePondServerConfig({
-                processUrl: '/admin/uploads/filepond/',
-                patchUrl: '/admin/uploads/filepond/',
-                revertUrl: '/admin/uploads/filepond/',
+                processUrl: '/admin/uploads/filepond',
+                patchUrl: '/admin/uploads/filepond',
+                revertUrl: '/admin/uploads/filepond',
             }),
         });
     }
